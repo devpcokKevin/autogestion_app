36d884a7afbff46ed4ccaaf1f3a9fddbfdc055e7
devpcokKevin <135277315+devpcokKevin@users.noreply.github.com>
2024-06-28 13:28:36 -0500

    Scaner con name y foto del trabajador

--- lib/screens/qrScanner/scanner_screen.dart
+++ lib/screens/qrScanner/scanner_screen.dart
@@ -1,11 +1,18 @@
+
 import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
+import 'package:autogestion/utils/constants.dart';
+import 'package:get/get.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:dio/dio.dart';
 import 'package:autogestion/screens/qrScanner/qroverlay.dart';
 import '../../shared/appbar.dart';
-import 'package:autogestion/utils/constants.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
+
+const bgColor = Color(0xffafafa);
 class QRScannerScreen extends StatefulWidget {
   final String appBarTitle;
   final IconData appBarIcon;
@@ -19,6 +26,7 @@ class QRScannerScreen extends StatefulWidget {
 class _QRScannerScreenState extends State<QRScannerScreen> {
   bool isScanCompleted = false;
   bool isScanningEnabled = true;
+  MobileScannerController controller = MobileScannerController();
   Timer? scanSuccessTimer;
 
   void closeScreen() {
@@ -36,7 +44,7 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
     });
   }
 
-  void showScanSuccessDialog(String code) {
+  void showScanSuccessDialog(String nombre, Uint8List? fotoBytes) {
     showDialog(
       context: context,
       barrierDismissible: true,
@@ -54,38 +62,83 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
             children: [
               SizedBox(height: 16),
               Text(
-                code,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                nombre,
+                style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
               ),
+              SizedBox(height: 16),
+              if (fotoBytes != null)
+                Container(
+                  width: 100,
+                  height: 100,
+                  child: Image.memory(fotoBytes, fit: BoxFit.cover),
+                ),
               SizedBox(height: 16),
               Icon(
                 Icons.check_circle,
                 color: Colors.green,
-                size: 50,
+                size: 30,
               ),
             ],
           ),
+
         );
       },
     );
   }
 
+  Uint8List hexToBytes(String hex) {
+    final buffer = Uint8List(hex.length ~/ 2);
+    for (int i = 0; i < hex.length; i += 2) {
+      buffer[i ~/ 2] = int.parse(hex.substring(i, i + 2), radix: 16);
+    }
+    return buffer;
+  }
+
   Future<void> sendQrDataToServer(String qrCode) async {
+    var dio = Dio();
+    var url = 'http://10.143.0.33:7259/api/Qr/QrFoto';
+    var data = {
+      "trabajador_id": 20019,
+      "empresa_codigo": "20354561124"
+    };
+
     try {
-      Dio dio = Dio();
       final response = await dio.post(
-        'https://localhost:7259/api/Qr/QrFoto',
-        data: {
-          "trabajador_id": 20019,
-          "empresa_codigo": "20354561124",
-        },
+        url,
         options: Options(
-          headers: {"Content-Type": "application/json"},
+          headers: {
+            "Content-Type": "application/json",
+          },
         ),
+        data: data,
       );
-      print(response.data[1]['rpta']);
+
+      if (response.statusCode == 200) {
+        print('Response status: ${response.statusCode}');
+        print('Response data: ${response.data}');
+
+        var responseData = response.data;
+        var item3 = responseData['item3'];
+        var nombreCompleto = item3['trabajador_nombre_completo'];
+        var fotoHex = item3['trabajador_foto'];
+        var fotoBytes = hexToBytes(fotoHex);
+
+        showScanSuccessDialog(nombreCompleto, fotoBytes);
+
+        Fluttertoast.showToast(
+          msg: 'Datos obtenidos correctamente',
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0,
+        );
+
+      } else {
+        print('Error: ${response.statusCode}');
+      }
     } catch (e) {
-      print("Error al enviar los datos del QR: $e");
+      print('Error: $e');
     }
   }
 
@@ -145,16 +198,13 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
                             isScanningEnabled = false;
                           });
 
-                          showScanSuccessDialog(code);
                           sendQrDataToServer(code);
                           startScanSuccessTimer();
                         }
                       },
                     ),
                   ),
-                  Positioned.fill(
-                    child: QRScannerOverlay(overlayColour: backgroundColorLight),
-                  ),
+                  const QRScannerOverlay(overlayColour: backgroundColorLight),
                 ],
               ),
             ),
